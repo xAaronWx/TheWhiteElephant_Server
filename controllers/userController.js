7c60ff238f46545c66590e3acfcef92b0d9ecc00
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-04-08 21:32:53 -0400

    completed userController

---
+++ controllers/userController.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { Router } = require("express");
+const { User } = require("../models");
+const validateSession = require("../middleware/validate-session");
+
+Router.post("/create", function (req, res) {
+  User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 15),
+  })
+    .then(function createSuccess(user) {
+      let token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: 60 * 60 * 24 }
+      );
+      res.json({
+        email: email,
+        message: "User Successfully Created",
+        sessionToken: token,
+      });
+    })
+    .catch(function (err) {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/login", function (req, res) {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then(function loginSuccess(user) {
+      if (user) {
+        bcrypt.compare(
+          req.body.password,
+          user.password,
+          function (err, matches) {
+            if (matches) {
+              let token = jwt.sign(
+                { id: user.id, email: user.email },
+                process.env.JWT_SECRET,
+                { expiresIn: 60 * 60 * 24 }
+              );
+              res.status(200).json({
+                email: email,
+                message: "User Successfully Logged in!",
+                sessionToken: token,
+              });
+            } else {
+              res.status(502).send({ error: "Login Failed" });
+            }
+          }
+        );
+      } else {
+        res.status(500).json({ error: "User does not exist" });
+      }
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
